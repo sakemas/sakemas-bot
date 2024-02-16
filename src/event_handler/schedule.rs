5b9506d8dbe68d0b847a4f20a7266db8777bb893
Mimomi <10952851+miiimooomiii@users.noreply.github.com>
2024-02-16 19:05:31 +0900

    chore: add println

--- src/event_handler/schedule.rs
+++ src/event_handler/schedule.rs
@@ -13,9 +13,15 @@ where
     let schedule = Schedule::from_str(cron).expect("Invalid cron expression");
     let ctx = Arc::clone(&ctx);
 
+    println!(
+        "Next 10 events: {:?}",
+        schedule.upcoming(Utc).take(10).collect::<Vec<_>>()
+    );
+
     let mut now = Utc::now();
     for next_event in schedule.upcoming(Utc).take(10) {
         let delay = next_event - now;
+        println!("Delay is {}", delay);
         time::sleep(delay.to_std().expect("Invalid Duration")).await;
         func(Arc::clone(&ctx));
         now = Utc::now();
