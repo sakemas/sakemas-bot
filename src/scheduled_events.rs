aa600268489ff499dc6070e05129a12d8116cbf5
Mimomi <10952851+miiimooomiii@users.noreply.github.com>
2024-02-17 11:30:47 +0900

    fix: summarize schedule fn

--- src/scheduled_events.rs
+++ src/scheduled_events.rs
@@ -8,19 +8,16 @@ use tokio::time;
 pub async fn schedule_vc_announcement(ctx: Arc<Context>, cron: &str) {
     let schedule = Schedule::from_str(cron).expect("Invalid cron expression");
 
+    let channel_id = std::env::var("TEST_VC_ANNOUNCEMENT_CHANNEL")
+        .expect("Expected TEST_VC_ANNOUNCEMENT_CHANNEL environment variable");
+    let channel_id = ChannelId::from_str(&channel_id).expect("Invalid ChannelId");
+
     let mut now = Utc::now();
     for next_event in schedule.upcoming(Utc).take(10) {
         let delay = next_event - now;
         time::sleep(delay.to_std().expect("Invalid Duration")).await;
-        announce_vc(&ctx).await;
+        let _ = channel_id.say(&ctx.http, "イベント告知メッセージ").await;
         println!("VC announcement complete");
         now = Utc::now();
     }
-}
-
-pub async fn announce_vc(ctx: &Context) {
-    let channel_id = std::env::var("TEST_VC_ANNOUNCEMENT_CHANNEL")
-        .expect("Expected TEST_VC_ANNOUNCEMENT_CHANNEL environment variable");
-    let channel_id = ChannelId::from_str(&channel_id).expect("Invalid ChannelId");
-    let _ = channel_id.say(&ctx.http, "イベント告知メッセージ").await;
 }
