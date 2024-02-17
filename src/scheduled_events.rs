815fd913a6a704238c58a9b319c4bf54b6e45d8e
Mimomi <10952851+miiimooomiii@users.noreply.github.com>
2024-02-17 13:16:43 +0900

    chore: replace expect() with unwrap()

--- src/scheduled_events.rs
+++ src/scheduled_events.rs
@@ -6,16 +6,15 @@ use std::sync::Arc;
 use tokio::time;
 
 pub async fn schedule_vc_announcement(ctx: Arc<Context>, cron: &str) {
-    let schedule = Schedule::from_str(cron).expect("Invalid cron expression");
+    let schedule = Schedule::from_str(cron).unwrap();
 
-    let channel_id = std::env::var("TEST_VC_ANNOUNCEMENT_CHANNEL")
-        .expect("Expected TEST_VC_ANNOUNCEMENT_CHANNEL environment variable");
-    let channel_id = ChannelId::from_str(&channel_id).expect("Invalid ChannelId");
+    let channel_id = std::env::var("TEST_VC_ANNOUNCEMENT_CHANNEL").unwrap();
+    let channel_id = ChannelId::from_str(&channel_id).unwrap();
 
     let mut now = Utc::now();
     for next_event in schedule.upcoming(Utc).take(10) {
         let delay = next_event - now;
-        time::sleep(delay.to_std().expect("Invalid Duration")).await;
+        time::sleep(delay.to_std().unwrap()).await;
         let _ = channel_id.say(&ctx.http, "イベント告知メッセージ").await;
         println!("VC announcement complete");
         now = Utc::now();
