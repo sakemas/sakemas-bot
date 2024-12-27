9efab5c6362246e4f7196a5c314ac9df9c0982c6
Mimomi <10952851+miiimooomiii@users.noreply.github.com>
2024-12-27 18:41:18 +0900

    refactor: improve error messages

--- src/utils/twitter/media.rs
+++ src/utils/twitter/media.rs
@@ -2,18 +2,30 @@ use std::io::Cursor;
 use poise::serenity_prelude::Attachment;
 use twapi_v2::{api::{Authentication, BearerAuthentication}, upload::{post_media_upload_append, post_media_upload_init, post_media_upload_finalize, media_category::MediaCategory, response::Response}, headers::Headers};
 
+#[derive(Debug, thiserror::Error)]
+pub enum MediaUploadError {
+    #[error("Attachment download error")]
+    AttachmentError,
+    #[error("Media upload init error: {0}")]
+    InitError(twapi_v2::error::Error),
+    #[error("Media upload append error: {0}")]
+    AppendError(twapi_v2::error::Error),
+    #[error("Media upload finalize error: {0}")]
+    FinalizeError(twapi_v2::error::Error),
+}
+
 pub async fn upload_media(
     token: &str,
     attachment: &Attachment,
     media_category: Option<MediaCategory>,
     additional_owners: Option<String>,
-) -> anyhow::Result<(Response, Headers)> {
+) -> Result<(Response, Headers), MediaUploadError> {
     // INIT
     let media_type = attachment
         .content_type
         .as_ref()
         .expect("attachment content type is missing");
-    let bytes = attachment.download().await?;
+    let bytes = attachment.download().await.map_err(|_| MediaUploadError::AttachmentError)?;
     let authentication = BearerAuthentication::new(token.to_owned());
 
     let data = post_media_upload_init::Data {
@@ -24,12 +36,12 @@ pub async fn upload_media(
     };
     let (response, _) = post_media_upload_init::Api::new(data)
         .execute(&authentication)
-        .await?;
+        .await.map_err(MediaUploadError::InitError)?;
     let media_id = response.media_id_string;
     tracing::info!(media_id = media_id, "post_media_upload_init");
 
     // APPEND
-    execute_append(&bytes, &authentication, &media_id).await?;
+    execute_append(&bytes, &authentication, &media_id).await.map_err(MediaUploadError::AppendError)?;
 
     // FINALIZE
     let data = post_media_upload_finalize::Data {
@@ -37,7 +49,7 @@ pub async fn upload_media(
     };
     let res = post_media_upload_finalize::Api::new(data)
         .execute(&authentication)
-        .await?;
+        .await.map_err(MediaUploadError::FinalizeError)?;
     tracing::info!(media_id = media_id, "post_media_upload_finalize");
     Ok(res)
 }
@@ -46,7 +58,7 @@ async fn execute_append(
     data: &[u8],
     authentication: &impl Authentication,
     media_id: &str,
-) -> anyhow::Result<()> {
+) -> Result<(), twapi_v2::error::Error> {
     let mut itr = data.chunks(5000000);
 
     for (i, chunk) in itr.by_ref().enumerate() {
