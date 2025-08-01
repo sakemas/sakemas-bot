38f9adfe1fa005db3e4774813166d80aa79963a1
Mimomi <10952851+miiimooomiii@users.noreply.github.com>
2025-08-01 16:03:05 +0900

    Move cron string to constant and update VC message
    The body is unnecessary since all key changes are captured in the
    subject line.

--- src/scheduled_events.rs
+++ src/scheduled_events.rs
@@ -6,8 +6,10 @@ use std::str::FromStr;
 use std::sync::Arc;
 use tokio::time;
 
-pub async fn schedule_vc_announcement(http: Arc<Http>, cron: &str) {
-    let schedule = Schedule::from_str(cron).unwrap();
+const FRIDAY_NIGHT: &str = "0 0 13 * * Fri *";
+
+pub async fn schedule_vc_announcement(http: Arc<Http>) {
+    let schedule = Schedule::from_str(FRIDAY_NIGHT).unwrap();
 
     let channel_id = Channel::VcAnnouncement.id();
 
@@ -15,7 +17,7 @@ pub async fn schedule_vc_announcement(http: Arc<Http>, cron: &str) {
     for next_event in schedule.upcoming(Utc).take(10) {
         let delay = next_event - now;
         time::sleep(delay.to_std().unwrap()).await;
-        let _ = channel_id.say(&http, "イベント告知メッセージ").await;
+        let _ = channel_id.say(&http, "@everyone 金曜日 22時定例、VC呑みの時間です！").await;
         info!("Scheduled event: {:?}", next_event);
         now = Utc::now();
     }
